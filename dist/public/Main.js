9e0634647e95193308ce99da7c963c01a5da28dc
Agustin-developed <84350784+Agustin-developed@users.noreply.github.com>
2022-08-24 13:02:09 -0300

    borre el dist

--- dist/public/Main.js
+++
@@ -1,9 +0,0 @@
-import {loadNotes, onNewnote} from "./socket.js";
-import {onHadleSubmit, renderNotes,appendNote} from "./ui.js"
-
-onNewnote(appendNote);
-loadNotes(renderNotes);
-
-const noteForm = document.querySelector('#noteForm');
-
-noteForm.addEventListener('submit',onHadleSubmit);
